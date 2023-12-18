425c70608dddbb26e6a87789b81538109485c3ec
beginnerunidue <42777842+beginnerunidue@users.noreply.github.com>
2023-12-18 23:38:08 +0100

    Created booksSlice instead of regular reducer

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 // import { v4 as uuidv4 } from "uuid";
-import { addBook } from "../../redux/books/actionCreators";
+import { addBook } from "../../redux/slices/booksSlice";
 import createBookWithID from "../../utils/createBookWithID";
 import booksData from "../../data/books.json";
 import "./BookForm.css";
